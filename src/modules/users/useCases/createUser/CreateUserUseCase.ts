f0f46627eeef74a1c4b7c0f2a1c6325b98e90862
jose <josesandeson54@gmail.com>
2021-09-09 10:37:18 -0300

    :sparkles: concluido usecase de showUserProfile

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { response } from "express";
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -10,6 +11,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
+    const userAlreadyExistis = this.usersRepository.findByEmail(email);
+
+    if(userAlreadyExistis){
+      throw new Error("User already exists!")
+    }
+
     return this.usersRepository.create({ 
        email,
        name
