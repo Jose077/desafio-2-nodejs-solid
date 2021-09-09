a5df731b8800c8e660fd5b2a4384579f5ae12d16
jose <josesandeson54@gmail.com>
2021-09-09 09:32:31 -0300

    :sparkles: finalizados as implementacoes de reposiorios

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -35,19 +35,32 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+     const user = this.users.find(e => e.id == id);
+
+     if(user) { 
+       return user
+     }
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find(e => e.email = email);
+
+    if(user){
+      return user
+    }
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = receivedUser;
+
+    user.admin = true;
+    user.created_at = new Date()
+
+    return user
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
